3dc91a624e9b1d1b0244e21565bdc5988c1a5e42
Crash0v3r1de <nb.faulds@gmail.com>
2021-04-18 10:43:30 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordMikrotikBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
